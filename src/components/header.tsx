b32b12fda65877bc256092555f541a21ed5aca73
Gerson C Filho <gersoncfilho@mac.com>
2024-05-03 08:34:56 +0100

    refact: style header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,14 @@ export default function Header() {
   return (
     <div className="w-full absolute text-white z-10">
       <nav className="container flex flex-wrap items-center justify-between p-4 mx-auto md:p-8">
-        <Link href="/">Home</Link>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
+        <div className="font-bold text-xl">
+          <Link href="/">Home</Link>
+        </div>
+        <div className="space-x-4 text-xl">
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
+          <Link href="/scale">Scale</Link>
+        </div>
       </nav>
     </div>
   );
